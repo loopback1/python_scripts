1ae30c0ec9f7150c540d145d73cfa61a565f09b3
loopback1 <secure101@outlook.com>
2017-06-16 09:56:12 -0500

    updating

--- compare_files/compare_ip_file_dumps_1.py
+++ compare_files/compare_ip_file_dumps_1.py
@@ -1,9 +1,8 @@
 import re
 
-def get_ip_from_file(file):
 
-    with open(file) as f:
-
+def get_ip_from_file(file_1):
+    with open(file_1) as f:
         x = [y.strip() for y in f]
 
         ip_list = [re.findall('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', t) for t in x]
@@ -11,33 +10,24 @@ def get_ip_from_file(file):
         return ip_list
 
 
+def parse(file_1):
+    filter_1 = get_ip_from_file(file_1)
 
-def parse(file):
-
-    filter = get_ip_from_file(file)
-
-    filter_finish =  {x for i in filter for x in i}
+    filter_finish = {x for i in filter_1 for x in i}
 
     return sorted(filter_finish)
-
 
 
 old = parse("C:\python27\ip.csv")
 
 new = parse("C:\python27\ip2.csv")
 
-
-
 report = [x for x in new if x in old]
-
-
 
 print "IPs in file2 that EXIST in file1:\n\n"
 
 for i in report:
-
     print i
-
 
 print "\n\nfile1 unique IPs: " + str(len(old))
 
@@ -45,4 +35,4 @@ print "file2 unique IPs: " + str(len(new))
 
 print "found: " + str(len(report))
 
-#test
+# test
---
+++ scraps/test2.py
@@ -0,0 +1,4 @@
+inventory = ["catalyst 3750", "catalyst 2950", "2911 router", "7965G phone"]
+
+for i in inventory:
+    print i
