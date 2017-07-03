08ce42e1b99bd0e7db07770ba70f7ec6a42ee6ed
loopback1 <jl78@outlook.com>
2017-07-03 18:17:06 -0500

    updating sms module

--- networking/security/check_for_host_sms.py
+++ networking/security/check_for_host_sms.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-# this is to alert when a particluar host is seeing on the network doing 
+# this is to alert when a particluar host is seeing on the network doing
 # uses smtplib to send sms using a gmail account
 # uses scapy module to sniff packets on the wire
 
@@ -20,16 +20,16 @@ pcap_filter = r'host 10.255.20.121 and dst port 53'
 
 
 def send_mail(body):
-    try:  
-        gmail_user = 'xxxxxx@gmail.com'  
+    try:
+        gmail_user = 'xxxxxx@gmail.com'
         gmail_password = 'xxxxx'
-        to = ['xxxxxx@mms.att.net']  
+        to = ['xxxxxx@mms.att.net']
         server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
         server.ehlo()
         server.login(gmail_user, gmail_password)
         server.sendmail(gmail_user, to, body)
         server.close()
-    except Exception as e:  
+    except Exception as e:
         print e
 
 
@@ -38,23 +38,26 @@ def schedule():
     day = datetime.datetime.today().weekday()
     return day, hour
 
+
 def check_1():
     day, hour = schedule()
     if (hour >= 8 and hour <= 19) and (day <= 4):
         send_mail('alert $check_1 .121')
-        
+
+
 def pkt_callback(pkt):
     check_1()
+
 
 def main():
     sniff(filter=pcap_filter, count=1, prn=pkt_callback)
 
 while True:
+
     try:
         main()
     except Exception as e:
         print e
     with open('/root/sms.log', 'a') as f:
         f.write(time.strftime('%c') + '\n')
-    time.sleep(5400)                           
-
+    time.sleep(5400)
