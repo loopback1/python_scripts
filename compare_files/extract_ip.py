95f657a9457038e8b3dda23822054bf066057fdb
loopback1 <jl78@outlook.com>
2017-07-07 23:37:38 -0500

    adding extract_ip.py

---
+++ compare_files/extract_ip.py
@@ -0,0 +1,16 @@
+import re
+import sys
+#from netaddr import CIDR, IP
+file = sys.argv[1]
+
+
+
+def get_ips(file):
+    with open(file) as f:
+        x = [x.strip() for x in f]
+        ips = [re.findall('[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}', t) for t in x]
+        clean_ips = {x for i in ips for x in i}
+        return list(clean_ips)
+
+print get_ips(file)
+
