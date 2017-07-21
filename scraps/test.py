9e39ecc4d6c7c2bdb8c9581fc2c01ef2d3bae977
loopback1 <jl78@outlook.com>
2017-07-20 23:23:02 -0500

    updating tmux.conf

--- scraps/test.py
+++ scraps/test.py
@@ -1,14 +1,1 @@
-
-
-import netmiko
-
-device= ''' 10.11.250.2
-10.12.250.2
-10.13.127.2
-10.13.15.2
-10.13.31.2
-10.13.47.2
-10.13.63.2 '''.strip().splitlines()
-
-for i in device:
-    print i
+# 
