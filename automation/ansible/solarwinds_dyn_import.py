310ca0a725e65d0583257b73b9b461ac64cb0bb5
lima <jl78@outlook.com>
2017-07-22 01:49:47 -0500

    adding solarwinds_dyn_import.py

---
+++ automation/ansible/solarwinds_dyn_import.py
@@ -0,0 +1,39 @@
+# jlima 
+# 7/22/2017
+# import devices from solarwinds using custom properties. swis queries can be modified
+# using windows swis tool from solarwinds, requires local read only account to run on 
+# needs work to separate host results by groups...
+# DEVICE_TYPE is a custom property assigned to nodes.. can be a regular attribute like Vendor etc..
+#
+# requires: https://github.com/solarwinds/orionsdk-python
+#
+
+import json
+import requests
+from orionsdk import SwisClient
+
+npm_server = '10.1.2.3'
+username = 'account'
+password = 'password'
+
+device_type = "'firewall'"
+
+verify = False
+if not verify:
+    from requests.packages.urllib3.exceptions import InsecureRequestWarning
+    requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+swis = SwisClient(npm_server, username, password)
+
+ios_router = swis.query( "SELECT TOP 1000 IPAddress FROM Orion.Nodes I WHERE I.CustomProperties.DEVICE_TYPE = " + device_type )
+
+data = {'_meta': {'hostvars': {}}, 'hosts' : []}
+
+def get_inv():
+    for i in ios_router['results']:
+        for k,v  in i.items():
+            data['hosts'].append(v)
+    print(json.dumps(data, sort_keys=True, indent=2))
+        
+get_inv()
+
