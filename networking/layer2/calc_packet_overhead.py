3cb392e6cbcf323711c8ffe13e86e36977eb57a7
lima@work <lima@work>
2018-05-23 15:43:07 -0500

    updating calc packet header

--- networking/layer2/calc_packet_overhead.py
+++ networking/layer2/calc_packet_overhead.py
@@ -1,40 +1,41 @@
 #!/usr/bin/env python
-'''calculate ethernet overhead
+'''calculate packet header overhead
 
 requires python3
 
 usage:
 
-python3 calc_line_overhead.py $1 $2 $3
+python3 calc_packet_overhead.py $1 $2 $3
 
     $1 = line speed in mbps
     $2 = average packet size expected
-    $3 = ethernet overhead e.g. 18 bytes for ethernet
+    $3 = ethernet overhead e.g. 18 bytes for ethernet +4 with dot1q
 
 use whole numbers!
 
 example:
 
-    python calc_line_overhead.py 500 128 18
+    python calc_packet_overhead.py 500 128 18
 
 500 = 500Mbps
 128 = 128 byte avg packet size expected
-18 = 14 byte ethernet header + 4 byte FCS frame(CRC32), dot1q = +4 bytes
+18 = 14 byte ethernet header + 4 byte FCS frame(CRC32), dot1q = +4 bytes (22)
 
 '''
+# jose lima  5.19.2018
 
 import sys
 
 
 def calc_ethernet_overhead(raw_line_throughput, avg_packet_size, overhead):
     ''' Calculates maximun thoughput on a line including additonal L2/ethernet overhead \
-    and returns recommended shaping policy based on the addtional overhead expected.
+    and returns appropiate shaping policy based on the addtional overhead expected.
 
     variable definitions:
 
         raw_line_throughput = in megabits/sec 500 for a 500Mbps line, 1000 for 1Gbps line etc.
         avg_packets_size = avg packet size expected on this line
-        overhead = type of overhead e.g. ethernet = (14 byte header + 4 byte FSC)
+        overhead = type of overhead e.g. ethernet = (14 byte header + 4 byte FCS)
 
     '''
 
@@ -47,22 +48,25 @@ def calc_ethernet_overhead(raw_line_throughput, avg_packet_size, overhead):
     max_throughput = max_packets_per_second * \
         avg_packet_size * 8 / (1000 * 1000)
 
-    effective_percentage = (int(max_throughput) / raw_line_throughput) * 100
+    effective_percentage = (max_throughput / raw_line_throughput) * 100
 
     # calcuate total throughput with overhead
     total_including_overhead = raw_line_throughput + \
-        int(raw_line_throughput - max_throughput)
+        (raw_line_throughput - max_throughput)
 
-    overhead_bits = total_including_overhead - int(raw_line_throughput)
+    # overhead_bits = total_including_overhead - int(raw_line_throughput)
+    overhead_bits = total_including_overhead - raw_line_throughput
 
     return print("Line speed: {} Mbps\n \
     Avg packet size expected on this line: {} Bytes\n \
-    Overhead using this packet size at full speed: {} Mbps\n \
+    Overhead using this packet size at full speed: ~ {} Mbps\n \
     Percentage available for use: {} %\n \
     QoS shaping policy needed to stay in boundaries: {} Mbps\n".format(raw_line_throughput,
-                                                                       avg_packet_size, overhead_bits,
-                                                                       int(effective_percentage),
-                                                                       int(max_throughput)))
+                                                                       avg_packet_size, round(
+                                                                           overhead_bits, 2),
+                                                                       int(
+                                                                           effective_percentage),
+                                                                       round(max_throughput, 2)))
 
 
 my_help = '''calculate ethernet overhead
@@ -71,7 +75,7 @@ requires python3
 
 usage:
 
-python3 calc_line_overhead.py $1 $2 $3
+python3 calc_packet_overhead.py $1 $2 $3
 
     $1 = line speed in mbps
     $2 = average packet size expected
@@ -81,7 +85,7 @@ use whole numbers!
 
 example:
 
-    python calc_line_overhead.py 500 128 18
+    python calc_packet_overhead.py 500 128 18
 
 500 = 500Mbps
 128 = 128 byte avg packet size expected
